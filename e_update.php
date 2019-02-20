29846615e82c788d139b842cc46c0a830b4c27f7
Gábor <yesszus@gmail.com>
2019-02-20 10:42:50 +0100

    Update e_update.php

--- e_update.php
+++ e_update.php
@@ -1,4 +1,4 @@
 <?php
 // Set this URL to the location of your eversion xml file
-$evrsn_url="http://yesszus.tk/xml/prune_users.xml";
+$evrsn_url="http://yesszus.bplaced.net/xml/prune_users.xml";
 ?>
